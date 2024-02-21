9e8deb43ca235ac7385fe8b54e8a36759ebd00c6
Vegadelalyra <lyradeperseo@gmail.com>
2024-02-20 19:46:23 -0500

    feat: django project with chat using channels v3 and django v4

--- web_fundamentals/asgi.py
+++ web_fundamentals/asgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack 
+from channels.auth import AuthMiddlewareStack
 # Channels supports Django standard Auth which checks client's session.
 # AuthMiddleware requires SessionMiddleware to function, which itself
 # requires CookieMiddleware. For convenience, these are also provided
--- web_fundamentals/settings.py
+++ web_fundamentals/settings.py
@@ -45,6 +45,12 @@ INSTALLED_APPS = [
 
 ASGI_APPLICATION = 'web_fundamentals.asgi.application'
 
+CHANNEL_LAYERS = {
+  'default': {
+    'BACKEND': 'channels.layers.InMemoryChannelLayer'
+  }
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
